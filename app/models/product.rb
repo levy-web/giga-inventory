21bf6b423ffb1ede6240a556bbf71303b4ca324e
levy <lnjiru43@gmail.com>
2025-02-02 23:23:10 +0300

    :art: :lipstick: show track of low quantities

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+    validates :name, :price, :quantity, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    # tracks if stock is low
+    def low_stock?
+        quantity <= 10
+    end
+
 end
