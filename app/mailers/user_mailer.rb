d9533f580662a6ec7cc57a59b5775d7bd617988a
levy <lnjiru43@gmail.com>
2025-02-03 02:05:02 +0300

    :sparkles: (mailer): implemented sinple email notification to admin user when stocks are low

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: ENV['MAILER_USERNAME']
+
+    def stock_notification
+        @user = ENV['MAILER_ADMIN_EMAIL']
+        @product = params[:product]
+        mail(to: @user, subject: "Check on stock levels for #{@product.name}")
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,20 @@ class Product < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+    after_save :check_low_stock
+
     # tracks if stock is low
     def low_stock?
         quantity <= 10
     end
 
+    private
+
+    # check if stock is low everytime you save a product
+    def check_low_stock
+        if low_stock?
+            # send email if stock is low
+            UserMailer.with(product: self).stock_notification.deliver_later  # changed @product to self
+        end
+    end
 end
